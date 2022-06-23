d5be368c970d9617e5f8446e0cf797148793ae90
Jacqueline Grout <jacgrout@gmail.com>
2022-06-23 14:44:54 +0100

    Change theme

--- R/create-plot.R
+++ R/create-plot.R
@@ -11,12 +11,14 @@ df = read_csv("data/weatherAUS.csv")
 
 city = "Newcastle"
 
-scatter_plot = 
-df %>%
-  filter(Location == city) %>%
-  ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(color="red") +
-  labs(title = city)
+scatter_plot =
+  df %>%
+  filter(Location == "Newcastle") %>%
+  ggplot(aes(x = Temp9am, y = Humidity9am)) +
+  geom_point(colour = "red") +
+  labs(title = "Humidity and Temperature for Newcastle") +
+  theme_minimal()
+
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
